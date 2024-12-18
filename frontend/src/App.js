c7e42d45f0cef511016f8644e0c2fdfc9cd7a6c5
stephendyoungjr <stephenyoung.np@outlook.com>
2024-12-18 14:21:58 -0500

    App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { useDispatch } from 'react-redux'
+
+import Navigation from "./components/Navigation";
+import MainPage from "./components/MainPage";
+import EventPage from "./components/EventPage";
+import SearchPage from "./components/SearchPage";
+import ProfilePage from "./components/ProfilePage";
+import Footer from "./components/Footer";
+
+import './css/index.css';
+
+import * as sessionActions from './store/session';
+
+function App() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(()=> {
+    if(isLoaded === false) {
+      dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+    }
+  }, [dispatch, isLoaded])
+
+  return (
+    <>
+      {isLoaded && (
+        <Switch>
+          <Route exact path='/'>
+            <div className={`app-grid-container`}>
+              <Navigation isLoaded={isLoaded} />
+              <MainPage />
+              <Footer />
+            </div>
+          </Route>
+           <Route exact path='/events/search'>
+            <div className={`app-grid-container`}>
+              <Navigation isLoaded={isLoaded} />
+              <SearchPage />
+              <Footer />
+            </div>
+          </Route>
+          <Route path='/events/:eventId'>
+            <div className={`app-grid-container`}>
+              <Navigation isLoaded={isLoaded} />
+              <EventPage />
+              <Footer />
+            </div>
+          </Route>
+          <Route path='/profile'>
+            <div className={`app-grid-container`}>
+              <Navigation isLoaded={isLoaded} />
+              <ProfilePage />
+              <Footer />
+            </div>
+          </Route>
+        </Switch>
+      )}
+    </>
+  );
+}
+
+export default App;
