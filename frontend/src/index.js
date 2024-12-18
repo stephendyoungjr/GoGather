70ae3e30b8d9e29f81cdd1d10a9113174c1229b6
stephendyoungjr <stephenyoung.np@outlook.com>
2024-12-18 14:32:11 -0500

    index.js udpate

---
+++ frontend/src/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider as ReduxProvider } from 'react-redux';
+
+import { ModalProvider } from './context/Modal'
+import App from './App';
+import configureStore from './store';
+import { csrfFetch, restoreCSRF } from './store/csrf';
+import * as sessionActions from './store/session';
+
+const store = configureStore();
+
+if(process.env.NODE_ENV !== 'production'){
+  restoreCSRF();
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+  window.sessionActions = sessionActions;
+}
+
+if (process.env.NODE_ENV !== "production") {
+  window.store = store;
+}
+
+function Root() {
+  return (
+    <ReduxProvider store={store}>
+      <ModalProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ModalProvider>
+    </ReduxProvider>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
