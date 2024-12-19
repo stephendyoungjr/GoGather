3d10d6156dfcd55efff4aa0e7d9c01fb30994312
stephendyoungjr <stephenyoung.np@outlook.com>
2024-12-19 08:29:20 -0500

    ssl

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,4 +1,15 @@
 module.exports = {
+  development: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, //  disables strict SSL verification
+      },
+    },
+  },
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
